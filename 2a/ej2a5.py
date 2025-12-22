fd7b5cd4e0beefe6b67a32fb013103b2b3ca7053
mariamiren <mcarmona8@uoc.edu>
2025-12-22 22:26:42 +0100

    Implement tests for linear regression results
    Added tests for linear regression and interpolation functions.

--- 2a/ej2a5.py
+++ 2a/ej2a5.py
@@ -31,18 +31,27 @@ from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
 import typing as t
+from ej2a5 import linear_regresion_and_interpolation
+def test_linear_regression_results():
+    data_x = np.linspace(0, 10, 100)
+    ata_y = 3 * data_x + 2 + np.random.normal(0, 0.5, 100)
+    results = linear_regression_and_interpolation(data_x, data_y)
 
+    #Asegurarse de que los resultados contengan los componentes esperados
+    assert "Linear_regression" in results
+    assert "interpolated_data" in results
 
-def linear_regression_and_interpolation(
-    data_x: t.List[float], data_y: t.List[float]
-) -> t.Dict[str, t.Any]:
-    # Write here your code
-    pass
+    #Verificar si la pendiente y la intersección estan en un rango esperado
+    assert 2.5 < results["linear_regression"]["slope"] < 3.5
+    assert 1.5 < results["linear_regression"]["intercept"] < 2.5
 
+#Test para verificar la longitug de los datos interpolados
+def test_interpolated_data_length():
+    data_x = np_linspace(0, 10, 100)
+    data_y = 3 * data_x + 2 + np.random.normal(0, 0.5, 100)
+    results = linear_regression_and_interpolation(data_x, data_y)
 
-def plot_results(data_x: t.List[float], data_y: t.List[float], results: t.Dict):
-    # Write here your code
-    pass
+    assert len(results["interpolated_data"]) == len(data_x
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
