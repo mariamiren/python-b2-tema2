841a63fa3569537eda99b95ec71d621d8af40709
mariamiren <mcarmona8@uoc.edu>
2025-12-22 17:49:50 +0100

    Implement tests for create and manipulate matrices
    Added tests for matrix creation and manipulation functions.

--- 2a/ej2a2.py
+++ 2a/ej2a2.py
@@ -45,18 +45,45 @@ Ejemplo:
 """
 import numpy as np
 import typing as t
-
+from eja2 import create_matrices, manipulate_matrices
 
 def create_matrices() -> t.Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    # Write here your code
-    pass
+    zeros_matrix, one_matrix, idetity_matrix = create_matrices()
+    
+    #Prueba para matriz de ceros
+    assert zeros_matrix.shape == (3, 3)
+    assert np.all(zeros_matrix == np.zeros((3,  3)))
 
+    #Prueba para matriz de unos
+    assert ones_matrix.shape == (2, 4)
+    assert np.all(ones_matrix == np.ones((2, 4)))
 
-def manipulate_matrices(
-    zeros_matrix: np.ndarray, ones_matrix: np.ndarray, identity_matrix: np.ndarray
-) -> t.Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    # Write here your code
-    pass
+    #Prueba para matriz identidad
+    assert identity_matrix.shape == (4, 4)
+    assert np.all(identity_matrix == np.eye(4))
+
+def manipulate_matrices():
+    zeros_matrix, ones_matrix, identity_matrix = create_matrices()
+    zeros_matrix_modif, ones_matrix_modif, identity_matrix_modif = manipulare_matrices(
+        zeros_matrix, ones_matrix, identity_matrix
+    )
+    #Prueba para la matriz de ceros modificada
+    expected_zeros_modif = np.zeros((3, 3))
+    expected_zeros_modif[1, 1] = 5
+    assert np.array_equal(zeros_matrix_modif, expected_zeros_modif)
+
+        #Prueba para la matriz de unos modificada
+    expected_ones_modif = np.ones((2, 4))
+    expected_ones_modif[:, 2] = 3
+    assert np.array_equal(ones_matrix_modif, expected_ones_modif)
+
+    #Prueba para la matriz de identidad modificada
+    expected_identity_modif = np.eye(4)
+    expected_identity_modif[np.arange(4), np.arange(3, -1, -1)] = 2
+    assert np.array_equal(identity_matrix_modif, expected_identity_modif)
+    
+    
+   
 
 
 # Para probar el código:
