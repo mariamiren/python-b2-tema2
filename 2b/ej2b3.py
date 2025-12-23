3e5f750937ceda65accae9d17579b09569a686d9
mariamiren <mcarmona8@uoc.edu>
2025-12-23 16:23:23 +0100

    Implement tests for read_sqlite_table and execute_sqlite_query
    Added test functions for reading and querying SQLite database.

--- 2b/ej2b3.py
+++ 2b/ej2b3.py
@@ -31,17 +31,28 @@ Salida esperada:
 import pandas as pd
 import sqlite3
 from pathlib import Path
+from ej2b3 import read_sqlite_table, execute_sqlite_query
+
+current_dir = Path(_file_).parent
+TEST_DB_PATH = current_dir / "dara/ej2b3/ramen-ratings.db"
+TEST_TABLE_NAME = "ramen_ratings"
+
+def read_sqlite_table():
+    df = read_sqlite_table(TEST_DB_PATH, TEST_TABLE_NAME)
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
 
 
-def read_sqlite_table(file_path: str, table_name: str) -> pd.DataFrame:
-    # Write here your code
-    pass
-
-
-def execute_sqlite_query(file_path: str, query: str) -> pd.DataFrame:
-    # Write here your code
-    pass
-
+def execute_sqlite_query():
+    query = "SELECT * FROM ramen_ratings WHERE Stars >=4"
+    df = execute_sqlite_query(TEST_DB_PATH, query)
+    df ["Stars"] = pd.to_numeric(df["Stars"], errors="coerce"
+    df = df.dropna(subset=["Stars"])
+    
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
+    assert all(df["Stars"] >= 4)
+    
 
 # Para probar el código, descomenta las siguientes líneas
 # db_path = "data/ej2b3/ramen-ratings.db"
