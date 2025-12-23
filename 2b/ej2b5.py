7bdb2a9d728df7aea22f9107cddfaaa3d92dfcb1
mariamiren <mcarmona8@uoc.edu>
2025-12-23 20:57:22 +0100

    Implement functions to read and validate population tables
    Added functions to read population data and validate tables.

--- 2b/ej2b5.py
+++ 2b/ej2b5.py
@@ -44,24 +44,38 @@ Salida esperada:
 
 import pandas as pd
 import typing as t
+import requests
+from ej2b5 import read_population_data, get_table_by_string_match, count_tables
 
+#URL for the Wikipedia page with population data
+URL = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
 
-def read_population_data(url: str, match_text: str = None) -> t.List[pd.DataFrame]:
-    # Write here your code
-    pass
+def read_population_data():
+    #Check if the Wikipedia page is reachable
+    responde = request.get(URL)
+    assert response.status_code == 200, "Wikipedia page not reachable"
 
+    #Check if tables can be read
+    tables = read_population_data(URL)
+    assert isinstance(tables, list), "Should return a list"
+    assert all(
+        isinstance(table, pd.DataFrame) for table in tables
+    ), "Each item should be a DataFrame"
 
-def get_table_by_string_match(
-    tables: t.List[pd.DataFrame], match_text: str
-) -> t.Union[pd.DataFrame, None]:
-    # Write here your code
-    pass
+def get_table_by_string_match():
+    tables = read_population_data(URL)
+    #Use a match_text that is known to exist in the tables
+    match_text = "Spain"
+    match_table = get_table_by_string_match(tables, match_text)
+    assert matched_table is not None, f"Should find a table with text {match_text}"
+    assert (
+        match_text in matched_table.to_string()
+    ), f"The table should contain the tect {match_text}"
 
-
-def count_tables(tables: t.List[pd.DataFrame]) -> int:
-    # Write here your code
-    pass
-
+def count_tables():
+    tables = read_population_data(URL)
+    assert isinstance(count_tables(tables), int), "Should return an integer"
+    assert count_tables(tables) > 0, "Should find one or more tables"
 
 # Para probar el código, descomenta las siguientes líneas
 # url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
