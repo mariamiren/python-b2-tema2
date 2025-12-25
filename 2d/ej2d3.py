223f2f8ecf5733ad52366ebbd36a2ec779c2a710
mariamiren <mcarmona8@uoc.edu>
2025-12-25 17:20:53 +0100

    Implement tests for linear regression functions
    Added pytest fixtures and tests for regression functions.

--- 2d/ej2d3.py
+++ 2d/ej2d3.py
@@ -34,25 +34,37 @@ from scipy.stats import linregress
 from typing import Tuple
 import numpy as np
 import matplotlib.pyplot as plt
+import pytest
+from ej2d3 import perform_linear_regression, plot_regression_line
 
-
+@pytest.fixture
+def housing_data():
+    current_dir = Path(__file).parent
+    data = pd.read_csv(current_dir / "data/housing.csv", skiprows=14)
+    return data
+    
 def perform_linear_regression(
-    data: pd.DataFrame, variable_1: str, variable_2: str
-) -> Tuple[float, float, float, float, float]:
-    # Write here your code
-    pass
+    slope, intercept, r_value, p_value, std_err = perform_linear_regression(
+        housing_data, "RM", "MEDV"
+    )
+    assert isinstance(slope, float), "The slope should be a float"
+    assert isinstance(intercept, float), "The intercept should be a float"
+    assert r_value**2 <= 1, "The r squared value should be lles tan or equal to 1"
+    assert 0 <= p_value <= 1, "The p value should be in the range[0, 1]"
+    assert isinstance(std_err, float), "The standard error should be a float"
 
-
-def plot_regression_line(
-    data: pd.DataFrame,
-    variable_1: str,
-    variable_2: str,
-    slope: float,
-    intercept: float,
-    return_fig_ax_test=False,
-):
-    # Write here your code
-    pass
+def plot_regression_line(housing_data)
+    slope: 8  #Use fictitious values for the clope and the intercept
+    intercept: -1
+    _, ax = plot_regression_line(
+        housing_data, "RM", "MEDV", slope, intercept, return_fig_ax_test=True
+    )
+    assert (
+        ax.get_title() == "Linear Regression between RM and MEDV"
+    ), "The chart title is not correct"
+    assert ax.get_xlabel() == "RM", "The X axis label is not correct"
+    assert ax.get_ylaberl() == "MEDV", "The Y axis label is not correct"
+    
 
 
 # Para probar el código, descomenta este código
