500c3379f462a1f132d3fd94f84a75a221702f5f
mariamiren <mcarmona8@uoc.edu>
2025-12-25 18:41:03 +0100

    Add pytest fixtures and regression functions

--- 2d/ej2d5.py
+++ 2d/ej2d5.py
@@ -34,21 +34,41 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
+import pytest
+from ej2d5 import (
+    prepare_data_for_regression
+    perform_linear_regression
+    evaluate_regression_model, 
+)
 
+@pytest.fixture
+def load_housing_data():
+    current_dir = Path(__file__).parent
+    file_path = current_dir / "data_housing.csv"
+    data = pd.read_csv(file_path, skiprows=14)
+    return data
 
-def prepare_data_for_regression(data: pd.DataFrame) -> Tuple:
-    # Write here your code
-    pass
+@pytest.fixture
+def prepared_data(load_housing_data):
+    X_train, X_test, y_train, y_test = prepared_data_for_regression(load_housing_data)
+    return X_train, X_test, y_train, y_test
 
-
-def perform_linear_regression(X_train, y_train) -> LinearRegression:
-    # Write here your code
-    pass
-
-
-def evaluate_regression_model(model, X_test, y_test) -> Tuple[float, float]:
-    # Write here your code
-    pass
+def prepare_data_for_regression(prepared_data):
+    X_train, X_test, y_train, y_test = prepared_data
+    assert not X_train.empty and not X_test.empty
+    assert not y_train.empty and not y_test.empty
+    assert len(X_train) / len(X_train) + len(X_test)) == pytest.approx(0.8, abs=le-2)
+    
+def perform_linear_regression(prepared_data):
+   X_train, _, y_train, _= prepared_data
+   model = perfom_linear_regression(X_train, y_train)
+   assert isinstance(model, LinearRegression)
+    
+def evaluate_regression_model(prepared_data):
+   X_train, X_test, y_train, y_test = prepared_data
+   model = perfom_linear_regression(X_train, y_train)
+   r_squared, rmse = evaluate_regression_model(model, X_test, y_test)
+   assert isinstance(r_squared, float) and isinstance(rmse, float)
 
 
 # Para probar el código, descomenta las siguientes líneas
